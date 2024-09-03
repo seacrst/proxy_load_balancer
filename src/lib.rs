2c2b61c4e9fd942a68ba3b58a540ebdb1e3b3d9f
Seacrest <paganseacrest@gmail.com>
2024-09-03 15:33:39 +0300

    round-robin

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use std::{str::FromStr, sync::Arc};
+
+use hyper::{client::ResponseFuture, Body, Client, Request, Response, Uri};
+use tokio::sync::RwLock;
+
+pub struct LoadBalancer {
+  client: Client<hyper::client::HttpConnector>,
+  workers: Vec<String>,
+  curr_worker: usize,
+}
+
+impl LoadBalancer {
+    pub fn new(workers: Vec<String>) -> Self {
+      Self {
+        workers,
+        curr_worker: 0,
+        client: Client::new()
+      }
+    }
+
+    pub fn forward_request(&mut self, req: Request<Body>) -> ResponseFuture {
+      let mut worker_uri = self.get_worker().to_owned();
+      if let Some(path_and_query) = req.uri().path_and_query() {
+          worker_uri.push_str(path_and_query.as_str());
+      }
+      let new_uri = Uri::from_str(&worker_uri).unwrap();
+      let headers = req.headers().clone();
+      let mut new_req = Request::builder()
+          .method(req.method())
+          .uri(new_uri)
+          .body(req.into_body())
+          .expect("request builder");
+
+      for (key, value) in headers.iter() {
+          new_req.headers_mut().insert(key, value.clone());
+      }
+
+      self.client.request(new_req)
+  }
+
+  // Round-robin strategy
+  fn get_worker(&mut self) -> &str {
+    let worker = self.workers.get(self.curr_worker).unwrap();
+    self.curr_worker = (self.curr_worker + 1) % self.workers.len();
+    worker
+  }
+}
+
+
+pub async fn handle_req(req: Request<Body>, ld_bal: Arc<RwLock<LoadBalancer>>) -> Result<Response<Body>, hyper::Error> {
+  ld_bal.write().await.forward_request(req).await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-use std::{convert::Infallible, net::SocketAddr};
+use std::{convert::Infallible, net::SocketAddr, sync::Arc};
 
-use hyper::service::service_fn;
+use hyper::{service::{make_service_fn, service_fn}, Server};
 
+use proxy_load_balancer::{handle_req, LoadBalancer};
 use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() {
-  let worker_hosts = vec![
-        "http://localhost:3000".to_string(),
-        "http://localhost:3001".to_string(),
-    ];
+  let workers = vec![
+    "http://localhost:3001".to_string(),
+    "http://localhost:3002".to_string(),
+  ];
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 1337));
+  let balancer = Arc::new(RwLock::new(LoadBalancer::new(workers)));
+  let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 1337));
+  let server = Server::bind(&addr)
+    .serve(
+      make_service_fn(move |_conn| {
+        let ld_bal = balancer.clone();
+        async move { Ok::<_, Infallible>(service_fn(move |req| handle_req(req, ld_bal.clone()))) }
+      })
+    );
 
-    let server = Server::bind(&addr).serve(make_service_fn(move |_conn| {
-      async move { Ok::<_, Infallible>(service_fn(move |req| handle(req))) }
-  }));
-}
-
-async fn handle(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-  Ok(())
+  if let Err(e) = server.await {
+    eprintln!("error: {}", e);
+  }
 }
